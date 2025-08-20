7c6d363aba2bd52a0201238e8895b7153732d48e
MannyUC <u3178302@uni.canberra.edu.au>
2025-08-20 18:58:45 +1000

    Add files via upload

---
+++ Week 4/Week 4 Tutorial Soloutions.py
@@ -0,0 +1,31 @@
+#Step 1 
+print("Welcome to the Python Coffee Shop!")
+
+#Step 2
+customer_name = input("What is your name?")
+print("Hello, " + customer_name + "! Let's order some coffee.")
+
+#Step 3
+price_coffee = 3.50
+price_latte = 4.00
+print("Coffee: $" + str(price_coffee))
+print("Latte: $" + str(price_latte))
+
+#Step 6
+menu_items = ["coffee", "latte", "espresso"]
+print ("Ourmenu:", menu_items)
+
+#Step 12
+print(str(468 + 751))
+print("How are you?")
+print(8 / 3, 4 * 7, 9 + 13, 2 ** 5, 6 * (3 + 2))
+size = 77
+x = 5
+y = 7
+print(abs(x - y) - 10)
+print(int(x ** 2) + 1.4)
+print(round(y + 3.14159, 2))
+
+a = 31
+b = 7
+print(a // b, a % b)
